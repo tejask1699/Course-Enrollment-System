c2965f6c965e1246482df168629f55978b308f0e
tejask1699 <t.kelaskar16@gmail.com>
2026-02-28 17:42:29 +0530

    Upgrade Next.js to latest secure version

--- client-full-stack/next.config.ts
+++ client-full-stack/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true, // disables lint errors blocking build
-  },
+
 };
 
 export default nextConfig;
